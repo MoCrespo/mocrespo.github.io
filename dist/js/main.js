f2343a0b99a59f316349322d2fb53f60e7b03cfc
MoCrespo <mcrespo495@gmail.com>
2021-04-27 04:22:38 +0200

    added menu-nav__item in function

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,6 +2,8 @@ const menuBtn = document.querySelector('.menu-btn')
 const hambuger = document.querySelector('.menu-btn__burger')
 const nav = document.querySelector('.nav')
 const menuNav = document.querySelector('.menu-nav')
+const navItems = document.querySelectorAll('.menu-nav__item')
+
 
 let showMenu = false
 
@@ -12,12 +14,15 @@ function toggleMenu() {
         hambuger.classList.add('open');
         nav.classList.add('open')
         menuNav.classList.add('open');
+        navItems.forEach(item => item.classList.add('open'));
 
         showMenu = true;
     } else {
         hambuger.classList.remove('open')
         nav.classList.remove('open')
         menuNav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'));
+
 
         showMenu = false
     }
